0b1abd2005daa5124206b5d723a79526a6a6ed6d
Vollate <uint44t@gmail.com>
2024-03-31 20:19:17 +0800

    init project

---
+++ LN-Chat_zh_CN.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="build/LN-Chat_autogen/include/ui_mainwindow.h" line="58"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="28"/>
+        <location filename="build/LN-Chat_autogen/include/ui_mainwindow.h" line="59"/>
+        <source>Hello world!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
